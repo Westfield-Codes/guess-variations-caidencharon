c51fee8bbf0d77e4c8e413568ac87f366780105f
caidencharon <caiden204899@student-schoolsofwestfield.org>
2024-10-08 13:21:18 +0000

    simple guess

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 // define function guessGame
 function guessGame() {
   // generate a random integer 1-100
-  let answer = Math.floor(Math.random()*100+1);
+  var answer = Math.floor(Math.random()*100+1);
   // display the integer for testing purposes only
   alert(answer);
   // create guess and set equal to 0 
   var guess = 0;
   // loop while guess is not equal to answer
-  while (guess != answer)}
+  while (guess != answer || guess == "q") {
     // set guess equal to asking user to "Guess a number, 1-100"
-   guess = prompt("Guess a number, 1-100");
+
     // if guess equals answer display "Correct!" 
-if (guess == answer) alert("Correct!");
+
     // or if guess is greater than answer display "Too high!" 
 
     // or if guess is less than answer display "Too low!" 
@@ -21,6 +21,6 @@ if (guess == answer) alert("Correct!");
     // else say "Bad input"
 
   // end the loop
-
+  }
 // end the function
 }
